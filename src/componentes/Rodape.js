76f3e118d7cb6034261f41e77eafcfe12fbc9de8
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-23 11:56:36 -0300

    criado componente Rodape

---
+++ src/componentes/Rodape.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Rodape = props => {
+    return (
+
+        <footer className='container-fluid text-center'>
+            <a href='#myPage' title='To Top'>
+                <span className='glyphicon glyphicon-chevron-up'></span>
+            </a>
+            <p>Projeto trabalhado no Curso de React JS - Por Carlos Henrique</p>
+            <p>Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
+        </footer>
+
+    )
+}
+
+export default Rodape;
