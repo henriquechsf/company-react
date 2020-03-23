288a8c2e172693dd14e5148c443b89060dc0d15a
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-23 16:23:36 -0300

    componente Servicos

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import Site from './Site'
 import Rodape from './componentes/Rodape'
+import Cabecalho from './componentes/Cabecalho';
+import Inicio from './componentes/Inicio';
+import Servicos from './componentes/Servicos';
 
 function App() {
   return (
     <div className="App">
+      <Cabecalho />
+      <Inicio />
+      <Servicos />
       <Site />
       <Rodape />
     </div>
