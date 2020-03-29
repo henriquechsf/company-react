e400e15c9b931c69f43023b204128b4bbc28e213
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-29 18:02:07 -0300

    gravando informações do Portfolio no Firebase

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -6,13 +6,62 @@ class AdminPortfolio extends Component {
     constructor(props) {
         super(props)
 
+        // bind para acessar o this em escopo diferente
+        this.gravaPortfolio = this.gravaPortfolio.bind(this)
+    }
+
+    // grava os dados no Firebase
+    gravaPortfolio(e) {
+        e.preventDefault()
+
+        // arquivo passa no input imagem
+        const arquivo = this.imagem.files[0]
+        const { name, size, type } = arquivo
+        console.log(name, size, type)
+
+        // referencia do arquivo
+        const ref = storage.ref(name)
+        // upload e retorno das informações da gravação
+        ref.put(arquivo)
+            .then(img => {
+                // pega a url da imagem gerada no firebase para salvar no site
+                img.ref.getDownloadURL()
+                    .then(downloadURL => {
+                        const novoPortfolio = {
+                            titulo: this.titulo.value,
+                            descricao: this.descricao.value,
+                            imagem: downloadURL
+                        }
+
+                        // gravação dos dados no Firebase
+                        config.push('portfolio', {
+                            data: novoPortfolio
+                        })
+                    })
+            })
     }
 
     render() {
         return (
+            <div style={{ padding: '0 120px' }}>
+                <h2>Portfófio - Área Administrativa</h2>
 
-            <h2>Portfófio - Área Administrativa</h2>
-
+                <form onSubmit={this.gravaPortfolio}>
+                    <div className="form-group">
+                        <label htmlFor="titulo">Título</label>
+                        <input type="text" className="form-control" id="titulo" ref={(ref) => this.titulo = ref} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="descricao">Descrição</label>
+                        <input type="text" className="form-control" id="descricao" ref={(ref) => this.descricao = ref} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="imagem">Imagem</label>
+                        <input type="file" className="form-control" id="imagem" ref={(ref) => this.imagem = ref} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Cadastrar</button>
+                </form>
+            </div>
         )
     }
 }
