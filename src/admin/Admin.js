f0a82be6f08232a6eb4c98ab9b3507176b021511
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-29 23:03:47 -0300

    protegendo as rotas administrativas

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+
+import { auth } from '../firebase-config'
 
 import AdminMenu from './AdminMenu';
 import AdminPortfolio from './AdminPortfolio';
@@ -8,9 +10,32 @@ class Admin extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            estaLogando: true,
+            estaAutenticado: false,
+            user: null
+        }
+    }
+
+    // ao carregar o componente
+    componentDidMount() {
+        auth.onAuthStateChanged(user => {
+            this.setState({
+                estaLogando: false,
+                estaAutenticado: !!user,
+                user
+            })
+        })
     }
 
     render() {
+        if (this.state.estaLogando) {
+            return <p>Aguarde...</p>
+        }
+        if (!this.state.estaAutenticado) {
+            return <Redirect to='/login' />
+        }
+
         // pasta raiz admin
         const path = this.props.match.url
 
