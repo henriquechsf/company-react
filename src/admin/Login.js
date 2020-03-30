d54b51500f369951a43fc3d4fb7f9dfd1e18d27d
Carlos Henrique <ch.ferreir@hotmail.com>
2020-03-29 22:45:29 -0300

    melhoria e validação na autenticação de login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 import { auth } from '../firebase-config'
 
 class Login extends Component {
     constructor(props) {
         super(props)
+
+        // estado
+        this.state = {
+            estaAutenticado: false,
+            estaLogando: false,
+            erro: false
+        }
 
         this.email = null
         this.senha = null
@@ -14,18 +22,24 @@ class Login extends Component {
     }
 
     autenticaUsuario() {
-        console.log(this.email.value, this.senha.value)
+        this.setState({ estaLogando: true })
         // autenticação
         auth.signInWithEmailAndPassword(this.email.value, this.senha.value)
             .then(user => {
                 console.log('Usuário logado', user)
+                this.setState({ estaAutenticado: true, estaLogando: false })
             })
             .catch(err => {
                 console.log('Erro', err)
+                this.setState({ erro: true, estaLogando: false })
             })
     }
 
     render() {
+        // encaminhar para Admin se estiver logado
+        if (this.state.estaAutenticado) {
+            return <Redirect to='/admin' />
+        }
         return (
 
             <div className="container" style={{ width: 500, marginTop: 70 }}>
@@ -38,9 +52,10 @@ class Login extends Component {
                 <div className="form-group">
                     <label htmlFor="senha">Senha</label>
                     <input type="password" name="senha" ref={ref => this.senha = ref} className="form-control" id="senha" placeholder="Digite a sua senha" />
+                    {this.state.erro && <small id="emailHelp" class="form-text text-muted">Email e/ou Senha incorretos.</small>}
                 </div>
 
-                <button type="button" onClick={this.autenticaUsuario} className="btn btn-primary">Entrar</button>
+                <button type="button" disabled={this.state.estaLogando} onClick={this.autenticaUsuario} className="btn btn-primary">Entrar</button>
 
             </div>
 
